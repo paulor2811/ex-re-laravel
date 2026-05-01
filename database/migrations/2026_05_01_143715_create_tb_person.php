d54faed68f6751556b0713f3ad7bee85fcb9e7d4
Paulo Ricardo <aleixopaulo22@gmail.com>
2026-05-01 11:56:16 -0300

    alterada a coluna id da tabela person para uuid

--- database/migrations/2026_05_01_143715_create_tb_person.php
+++ database/migrations/2026_05_01_143715_create_tb_person.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tb_person', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
             $table->string('name');
             $table->string("email")->unique();
             $table->softDeletes();
