e23d391cf522e6ec0f8c9b3cbe2d71212aab0440
Paulo Ricardo <aleixopaulo22@gmail.com>
2026-05-01 12:32:54 -0300

    Criado a coluna password

--- database/migrations/2026_05_01_143715_create_tb_person.php
+++ database/migrations/2026_05_01_143715_create_tb_person.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->uuid("id")->primary();
             $table->string('name');
             $table->string("email")->unique();
+            $table->string('password');
             $table->softDeletes();
             $table->timestamps();
         });
